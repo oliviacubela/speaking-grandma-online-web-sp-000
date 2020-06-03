14f13165e4ea72e707380dbc7821d6c9428ee564
Olivia Cubela <oliviacubela@gmail.com>
2020-06-03 02:08:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
     else
-  		return "I LOVE YOU GRANDMA!"
+  		return "HUH?! SPEAK UP, SONNY!"
     end
 end
 
